3f235438e8d2abb780ef4e1fb2b6808912eeccac
Ryan LaBouve <imryanlabouve@gmail.com>
2016-12-26 00:15:42 -0600

    Fix error in route test

--- tests/unit/application/route-test.js
+++ tests/unit/application/route-test.js
@@ -3,6 +3,10 @@ import { moduleFor, test } from 'ember-qunit';
 moduleFor('route:application', 'Unit | Route | application', {
   // Specify the other units that are required for this test.
   // needs: ['controller:foo']
+  needs: [
+    'service:metrics',
+    'ember-metrics@metrics-adapter:google-analytics'
+  ]
 });
 
 test('it exists', function(assert) {
