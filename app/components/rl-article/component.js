575bf9015248a023bf420de0a003dabc73e24341
Ryan LaBouve <imryanlabouve@gmail.com>
2016-12-27 00:43:01 -0600

    Fix silly JSHint errors

--- app/components/rl-article/component.js
+++ app/components/rl-article/component.js
@@ -1,9 +1,10 @@
+/* globals hljs */
 import Ember from 'ember';
 
 export default Ember.Component.extend({
   didInsertElement() {
     this.$('pre code').toArray().forEach((block) => {
-      $(block).addClass("ba b--black-10");
+      this.$(block).addClass("ba b--black-10");
       hljs.highlightBlock(block);
     });
 
